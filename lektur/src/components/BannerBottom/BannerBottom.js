8357acfeb8a9f289ce26e903ecc5d4bc3c4ebdd0
BagusNugrohow <papaobobo@gmail.com>
2022-02-02 23:46:35 +0700

    fix something in bottom banner

--- lektur/src/components/BannerBottom/BannerBottom.js
+++ lektur/src/components/BannerBottom/BannerBottom.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "../../styles/BannerBottom.module.css";
 import Teacher from "../../assests/teacher-banner.png";
 
 const BannerBottom = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <div className={styles["banner-bottom"]}>
-      <div className={styles.left}>
-        <h2 className={styles.title}>Create Your Own Class</h2>
-        <a className={styles.btn} href="/">
-          Register Now
-        </a>
-      </div>
-      <div class={styles.right}>
-        <img src={Teacher} alt="Teacher" />
-      </div>
-    </div>
+    <>
+      {!isLogin ? (
+        <div className={styles["banner-bottom"]}>
+          <div className={styles.left}>
+            <h2 className={styles.title}>Create Your Own Class</h2>
+            <a className={styles.btn} href="/">
+              Register Now
+            </a>
+          </div>
+          <div class={styles.right}>
+            <img src={Teacher} alt="Teacher" />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
