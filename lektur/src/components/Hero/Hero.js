890e0423a572941a0f0042230bdf3e5f5b694f75
BagusNugrohow <papaobobo@gmail.com>
2022-02-02 21:50:58 +0700

    mengerjakan video player di hero Component

--- lektur/src/components/Hero/Hero.js
+++ lektur/src/components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 import styles from "../../styles/Hero.module.css";
 
@@ -6,13 +7,15 @@ const Hero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles["content-container"]}>
-        <div className={styles.title}>
+        <div className={styles.left}>
           <h1>Bring your class at home</h1>
           <a href="" className={styles["hero-btn"]}>
             Enroll Now
           </a>
         </div>
-        <div className={styles["video-section"]}></div>
+        <div className={styles.right}>
+          <ReactPlayer className={styles["react-player"]} url="https://www.youtube.com/watch?v=ysz5S6PUM-U" width="100%" height="70%" />
+        </div>
       </div>
     </div>
   );
