1a32a2ab9f49ee653bbf60fe8c1fa0c09b7241cf
BagusNugrohow <papaobobo@gmail.com>
2022-02-06 18:30:54 +0700

    fix layout course content

--- lektur/src/components/ContentVideoMain/ContentVideoMain.js
+++ lektur/src/components/ContentVideoMain/ContentVideoMain.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 
 import styles from "../../styles/ContentVideoMain.module.css";
 
 const ContentVideoMain = () => {
   return (
     <main className={styles.main}>
-      <div className={styles["main-top"]}></div>
+      <div className={styles.container}>
+        <Breadcrumb className={styles.breadcrumb}>
+          <Breadcrumb.Item href="#" active>
+            React Crash Cource
+          </Breadcrumb.Item>
+          <Breadcrumb.Item href="#">Lesson #1 : What is React?</Breadcrumb.Item>
+        </Breadcrumb>
+        <h1 className={styles.title}>Lesson #1 : What is React?</h1>
+        <div className={styles["course-wrapper"]}>
+          <section className={styles.content}></section>
+          <aside className={styles.aside}></aside>
+        </div>
+      </div>
     </main>
   );
 };
