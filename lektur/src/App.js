abd26e619260cfceb2a74c5a4b451fb7c01a6005
BagusNugrohow <papaobobo@gmail.com>
2022-02-06 21:20:50 +0700

    fix setup

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import ContentModal from "./components/CourseModal/ContentModal";
-
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <ContentModal />
     </div>
   );
 }
