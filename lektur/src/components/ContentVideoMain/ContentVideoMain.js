e5316a7b11c46a79d34cf042b4e9136befd5db44
BagusNugrohow <papaobobo@gmail.com>
2022-02-06 22:10:58 +0700

    finish but not yet content/video page

--- lektur/src/components/ContentVideoMain/ContentVideoMain.js
+++ lektur/src/components/ContentVideoMain/ContentVideoMain.js
@@ -8,6 +8,18 @@ import playWhite from "../../assests/play-white.svg";
 import play from "../../assests/on-play.svg";
 import lock from "../../assests/lock.svg";
 import check from "../../assests/green-check.svg";
+import Card from "../../components/CourseCards/Card";
+
+let dummyData = {
+  src: "https://images.unsplash.com/photo-1643662372195-49a2b4ab6278?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  title: "Create Cinematic Music Video",
+  author: "John Doe",
+  videos: 15,
+  materials: 3,
+  description:
+    "Vestibulum fusce parturient urna a molestie orci. Lectus id quisque amet et vel elementum morbi cursus. Amet sagittis semper mauris diam orci facilisis...",
+  category: "Art & Humanity",
+};
 
 const ContentVideoMain = () => {
   return (
@@ -102,7 +114,47 @@ const ContentVideoMain = () => {
         </div>
         {/* Course Video And Play list end*/}
       </div>
-      <div className={styles.cards}></div>
+      <div className={styles.cards}>
+        <h2>Related Course</h2>
+        <div className={styles["card-list"]}>
+          <Card
+            src={dummyData.src}
+            title={dummyData.title}
+            author={dummyData.author}
+            videos={dummyData.videos}
+            materials={dummyData.materials}
+            description={dummyData.description}
+            category={dummyData.category}
+          />
+          <Card
+            src={dummyData.src}
+            title={dummyData.title}
+            author={dummyData.author}
+            videos={dummyData.videos}
+            materials={dummyData.materials}
+            description={dummyData.description}
+            category={dummyData.category}
+          />
+          <Card
+            src={dummyData.src}
+            title={dummyData.title}
+            author={dummyData.author}
+            videos={dummyData.videos}
+            materials={dummyData.materials}
+            description={dummyData.description}
+            category={dummyData.category}
+          />
+          <Card
+            src={dummyData.src}
+            title={dummyData.title}
+            author={dummyData.author}
+            videos={dummyData.videos}
+            materials={dummyData.materials}
+            description={dummyData.description}
+            category={dummyData.category}
+          />
+        </div>
+      </div>
     </main>
   );
 };
--- lektur/src/pages/ContentVideo.js
+++ lektur/src/pages/ContentVideo.js
@@ -3,10 +3,11 @@ import React from "react";
 import Navbar from "../components/Header/Navbar";
 import ContentVideoMain from "../components/ContentVideoMain/ContentVideoMain";
 import Footer from "../components/Footer";
+import styles from "../styles/ContentVideo.module.css";
 
 const ContentVideo = () => {
   return (
-    <div>
+    <div className={styles["content-video"]}>
       <Navbar />
       <ContentVideoMain />
       <Footer />
