4174c8de14635c81735a084816bb5efec931eab9
BagusNugrohow <papaobobo@gmail.com>
2022-02-06 21:49:15 +0700

    fix app

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import ContentVideo from "./pages/ContentVideo";
 
 import Home from "./pages/Home";
@@ -7,9 +6,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      {/* <ContentVideo /> */}
-      {/* <Home /> */}
-      <Footer />
+      <ContentVideo />
     </div>
   );
 }
