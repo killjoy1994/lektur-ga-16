c4a21bfc5b41dccffcd616565a40a25a5a1f33bb
BagusNugrohow <papaobobo@gmail.com>
2022-02-08 00:39:58 +0700

    apply styling  in homepage

--- lektur/src/pages/Home.js
+++ lektur/src/pages/Home.js
@@ -5,6 +5,7 @@ import Hero from "../components/Hero/Hero";
 import BannerBottom from "../components/BannerBottom/BannerBottom";
 import Navbar from "../components/Header/Navbar";
 import Footer from "../components/Footer";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
