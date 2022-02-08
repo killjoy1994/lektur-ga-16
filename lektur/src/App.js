56d54e3306ff9e73faa379964fd6ee22449ea013
BagusNugrohow <papaobobo@gmail.com>
2022-02-08 21:37:24 +0700

    try add course/content page

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+import ContentVideo from "./pages/ContentVideo";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <ContentVideo />
     </div>
   );
 }
