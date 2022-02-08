b39224fe289d0b5028a8ca3c4e23af0dc33222db
BagusNugrohow <papaobobo@gmail.com>
2022-02-08 13:53:56 +0700

    try content video

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import ContentVideo from "./pages/ContentVideo";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ContentVideo />
     </div>
   );
 }
