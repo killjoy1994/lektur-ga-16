f9d9c5f60ba94c5b7ee8f51ade30285c6d6540d8
BagusNugrohow <papaobobo@gmail.com>
2022-02-08 19:14:22 +0700

    Merge branch 'development' of gitlab.com:binarxglints_batch16/finalproject/lektur/fe_lektur into feature/Course/Content/Video

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/signup" element={} /> */}
       </Routes>
     </div>
   );
