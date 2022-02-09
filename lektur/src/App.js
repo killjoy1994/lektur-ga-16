4875a44b801cf87909123054221e7448187fa432
BagusNugrohow <papaobobo@gmail.com>
2022-02-09 11:43:46 +0700

    fix progress bar margin

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import DashboardCourse from "./components/DashboardCourse/DashboardCourse";
 import Home from "./pages/Home";
 import StudentDashboard from "./pages/StudentDashboard";
+import ContentVideo from "./pages/ContentVideo";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/course-content" element={<ContentVideo />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
       </Routes>
     </div>
