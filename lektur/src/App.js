d1698d0f88187319957d7df5eabad52012850551
Ardian ANG <ardianshalihin1@gmail.com>
2022-02-09 11:43:19 +0800

    initial commit studen/assessment

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import Footer from "./components/Footer";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Detail from "./pages/Detail";
-
 import Navbar from "./components/Header/Navbar";
+import FinalAssessment from "./pages/studentAssessment/FinalAssessment";
+
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FinalAssessment/>
       {/* <Home /> */}
-      <Detail/>
+      {/* <Detail/> */}
       <Footer />
     </div>
   );
---
+++ lektur/src/pages/studentAssessment/FinalAssessment.js
@@ -0,0 +1,221 @@
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+
+import styles from "../../styles/FinalAssessment.module.css";
+function FinalAssessment() {
+  return (
+    <div style={{ background: "#fffafa", height: "180vh" }}>
+      <header className={styles.container_head}>
+        <Breadcrumb className={styles.breadcrumb}>
+          <Breadcrumb.Item href="#" active>
+            React Crash Cource
+          </Breadcrumb.Item>
+          <Breadcrumb.Item href="#">Final Assessment</Breadcrumb.Item>
+        </Breadcrumb>
+        <h1 className={styles.title}>Final Assessment</h1>
+      </header>
+      <div className={styles.assessment_box}>
+        <h6>5 Questions</h6>
+        <div className={styles.question_list}> 
+          <div className={styles.box_question}>
+            1.
+            <p className={styles.question}>Neque eu lorem ultrices id. Et mattis lacus fermentum id nec, aenean enim, curabitur. Enim, donec quis odio ut enim scelerisque id erat laoreet?</p>
+          </div>
+          <p className={styles.answer}>Answer</p>
+          <div className={styles.answer_list}>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option3" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option4" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option5" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className={styles.question_list}>
+          <div className={styles.box_question}>
+            2.
+            <p className={styles.question}>Neque eu lorem ultrices id. Et mattis lacus fermentum id nec, aenean enim, curabitur. Enim, donec quis odio ut enim scelerisque id erat laoreet?</p>
+          </div>
+          <p className={styles.answer}>Answer</p>
+          <div className={styles.answer_list}>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option3" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option4" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option5" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className={styles.question_list}>
+          <div className={styles.box_question}>
+            3.
+            <p className={styles.question}>Neque eu lorem ultrices id. Et mattis lacus fermentum id nec, aenean enim, curabitur. Enim, donec quis odio ut enim scelerisque id erat laoreet?</p>
+          </div>
+          <p className={styles.answer}>Answer</p>
+          <div className={styles.answer_list}>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option3" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option4" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option5" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className={styles.question_list}>
+          <div className={styles.box_question}>
+            4.
+            <p className={styles.question}>Neque eu lorem ultrices id. Et mattis lacus fermentum id nec, aenean enim, curabitur. Enim, donec quis odio ut enim scelerisque id erat laoreet?</p>
+          </div>
+          <p className={styles.answer}>Answer</p>
+          <div className={styles.answer_list}>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option3" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option4" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option5" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className={styles.question_list}>
+          <div className={styles.box_question}>
+            5.
+            <p className={styles.question}>Neque eu lorem ultrices id. Et mattis lacus fermentum id nec, aenean enim, curabitur. Enim, donec quis odio ut enim scelerisque id erat laoreet?</p>
+          </div>
+          <p className={styles.answer}>Answer</p>
+          <div className={styles.answer_list}>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option3" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option4" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option5" />
+              <label className="form-check-label" for="exampleRadios1">
+                Et mattis lacus fermentum id nec
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.button}>
+        <button>Submit Assessment</button>
+      </div>
+    </div>
+  );
+}
+
+export default FinalAssessment;
