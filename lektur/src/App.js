fe80786837f044ea9d3082586ab53ed64356e659
BagusNugrohow <papaobobo@gmail.com>
2022-02-11 10:37:14 +0700

    add routes

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,14 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import ErrorPage from './errorPage/ErrorPage'
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+import Detail from "./pages/Detail";
+import ContentVideo from "./pages/ContentVideo";
+import FinalAssessment from "./pages/studentAssessment/FinalAssessment";
+import ErrorPage from "./errorPage/ErrorPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ErrorPage />} />
-        {/* <Route path="/signup" element={} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/final-assessment" element={<FinalAssessment />} />
+        <Route path="/course-content-video" element={<ContentVideo />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
--- lektur/src/errorPage/ErrorPage.js
+++ lektur/src/errorPage/ErrorPage.js
@@ -1,16 +1,18 @@
-function Errorpage () {
-    return (
-        <div className='not-found'>
-            <div className="notfound">
-                <div className="notfound-404">
-                    <h1>404</h1>
-                    <h2>Page not found</h2>
-                    <p>We couldn't find this page.</p>
-                </div>
-                <a href="#">Back to homepage</a>
-            </div>
+import styles from "../styles/Error.module.css";
+
+function Errorpage() {
+  return (
+    <div className={styles["not-found"]}>
+      <div className={styles.notfound}>
+        <div className={styles["notfound-404"]}>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <p>We couldn't find this page.</p>
         </div>
-    );
+        <a href="#">Back to homepage</a>
+      </div>
+    </div>
+  );
 }
 
 export default Errorpage;
