a269d07076bfb7e2bd19aaab4ea0af3659fe9825
BagusNugrohow <papaobobo@gmail.com>
2022-02-11 16:56:54 +0700

    add student dashboard route

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,13 +1,8 @@
-
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import DashboardCourse from "./components/DashboardCourse/DashboardCourse";
-import Home from "./pages/Home";
-<<<<<<< HEAD
 import StudentDashboard from "./pages/StudentDashboard";
-import ContentVideo from "./pages/ContentVideo";
-=======
+import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Detail from "./pages/Detail";
@@ -15,24 +10,18 @@ import ContentVideo from "./pages/ContentVideo";
 import FinalAssessment from "./pages/studentAssessment/FinalAssessment";
 import ErrorPage from "./errorPage/ErrorPage";
 
->>>>>>> c9dc85d163cc58f42eea452bc3fd1def5df11e1b
-
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/course-content" element={<ContentVideo />} />
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/final-assessment" element={<FinalAssessment />} />
         <Route path="/course-content-video" element={<ContentVideo />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="*" element={<ErrorPage />} />
->>>>>>> c9dc85d163cc58f42eea452bc3fd1def5df11e1b
       </Routes>
     </div>
   );
