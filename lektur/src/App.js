7e01023d30b9da495d891dc1ab89cba92a8f0ccb
BagusNugrohow <papaobobo@gmail.com>
2022-02-11 17:25:57 +0700

    activing routes

--- lektur/src/App.js
+++ lektur/src/App.js
@@ -1,4 +1,4 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import StudentDashboard from "./pages/StudentDashboard";
