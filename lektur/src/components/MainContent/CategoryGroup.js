9312ded6936581cbc92b6d4c1f21a1e8aa0f9bfe
BagusNugrohow <papaobobo@gmail.com>
2022-02-12 03:50:38 +0700

    try swipper

--- lektur/src/components/MainContent/CategoryGroup.js
+++ lektur/src/components/MainContent/CategoryGroup.js
@@ -1,23 +1,22 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
-// import required modules
-import { Navigation } from "swiper";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { FreeMode, Pagination } from "swiper";
 
 import styles from "../../styles/CategoryGroup.module.css";
 
 const CategoryGroup = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
-  useEffect(() => {});
-
   return (
     <div className={styles["category-group"]}>
-      <a href="#">BUSINESS</a>
+      {/* <a href="#">BUSINESS</a>
       <a href="#">TECHNOLOGY</a>
       <a href="#">GRAPHIC DESIGN</a>
       <a href="#">BUSINESS</a>
@@ -28,24 +27,31 @@ const CategoryGroup = () => {
       <a href="#">TECHNOLOGY</a>
       <a href="#">BUSINESS</a>
       <a href="#">TECHNOLOGY</a>
-      <a href="#">GRAPHIC DESIGN</a>
-      {/* <>
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          <SwiperSlide>
-            <a href="#">BUSINESS</a>
-          </SwiperSlide>
-          <SwiperSlide>
-            <a href="#">TECHNOLOGY</a>
-          </SwiperSlide>
-          <SwiperSlide></SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
-      </> */}
+      <a href="#">GRAPHIC DESIGN</a> */}
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className={styles.mySwiper}
+      >
+        <SwiperSlide>
+          <a href="#">BUSINESS</a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="#">TECHNOLOGY</a>
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
