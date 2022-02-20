f66c8eeff6b0c7f32644bdf1675dd0a2c02c652d
BagusNugrohow <papaobobo@gmail.com>
2022-02-21 03:49:35 +0700

    lift state up to MainContent, and fix hero and cards margin

--- src/components/CourseCards/Cards.js
+++ src/components/CourseCards/Cards.js
@@ -3,42 +3,26 @@ import { Link } from "react-router-dom";
 
 import Card from "./Card";
 import styles from "../../styles/Cards.module.css";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getCoursesAction } from "../../redux/actions/Courses/getCoursesAction";
 
-const Cards = () => {
-  const { courseList, isLoading, error } = useSelector((state) => state.courses);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getCoursesAction());
-  }, [dispatch]);
-
+const Cards = ({ courseList }) => {
   return (
     <div className={styles.cards}>
-      {isLoading && !error ? (
-        <p>Loading</p>
-      ) : (
-        courseList.map((course) => {
-          return (
-            <Link to={"/detail/" + course.id} key={course.id}>
-              <Card
-                src={course.image}
-                title={course.title}
-                author={course.by.fullName}
-                videos={"15"}
-                materials={"4"}
-                description={course.description}
-                category={course.category.name}
-                id={course.id}
-              />
-            </Link>
-          );
-        })
-      )}
-      {error && <p>Maaf ada kesalahan</p>}
+      {courseList.map((course) => {
+        return (
+          <Link to={"/detail/" + course.id} key={course.id}>
+            <Card
+              src={course.image}
+              title={course.title}
+              author={course.by.fullName}
+              videos={"15"}
+              materials={"4"}
+              description={course.description}
+              category={course.category.name}
+              id={course.id}
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 };
