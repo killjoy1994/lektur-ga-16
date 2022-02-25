c525bb14d81ddafa66a981af1078d2bd2d75d666
Ardian ANG <ardianshalihin1@gmail.com>
2022-02-25 23:34:29 +0800

    update responsive navbar

--- src/components/Header/NavbarComponent.js
+++ src/components/Header/NavbarComponent.js
@@ -73,6 +73,14 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto navbar-right">
+              <form className="input-group wrapper-search mobile" onSubmit={submitSearchText}>
+                <input className="form-control border-end-0 border" type="search" value={searchText} placeholder="Search course or lecturer" onChange={(evt) => setSearchText(evt.target.value)} />
+                <span className="input-group-append">
+                  <button className="btn btn-searching border-start-0 border ms-n5" type="button">
+                    <i className="fa fa-search" onClick={submitSearchText}></i>
+                  </button>
+                </span>
+              </form>
             <NavDropdown title="Category" id="basic-nav-dropdown">
               {uniqCategory.map((category, index) => (
                 <NavDropdown.Item href={"/categories/" + category} key={index}>
