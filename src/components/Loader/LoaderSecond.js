7150828629fccac6c4d773557c04c377a6f7f0a4
Ardian ANG <ardianshalihin1@gmail.com>
2022-02-26 22:33:21 +0800

    update tampilan dan responsive navbar

---
+++ src/components/Loader/LoaderSecond.js
@@ -0,0 +1,10 @@
+
+import styles from './LoaderSecond.module.css'
+
+export default function LoaderSecond () {
+  return (
+    <div className={styles['pos-center']}>
+     <div className={styles['loader-second']}></div>
+    </div>
+  )
+}
