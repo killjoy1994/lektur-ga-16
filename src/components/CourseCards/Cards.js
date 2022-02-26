27d2d5135aed8bb5f4900d0af885d9306f686557
BagusNugrohow <papaobobo@gmail.com>
2022-02-26 22:05:23 +0700

    fix api consime on each cards

--- src/components/CourseCards/Cards.js
+++ src/components/CourseCards/Cards.js
@@ -5,17 +5,29 @@ import Card from "./Card";
 import styles from "../../styles/Cards.module.css";
 
 const Cards = ({ courseList }) => {
+  
   return (
     <div className={styles.cards}>
       {courseList.map((course) => {
+        let videosCount = 0;
+        course.contents.forEach(content => {
+          return videosCount++ 
+        })
+
+        let materialsCount = 0;
+        course.contents.forEach(material => {
+          console.log(material)
+          return materialsCount += material.materials.length
+        })
+
         return (
           <Link to={"/detail/" + course.id} key={course.id}>
             <Card
               src={course.image}
               title={course.title}
               author={course.by.fullName}
-              videos={"15"}
-              materials={"4"}
+              videos={videosCount}
+              materials={materialsCount}
               description={course.description}
               category={course.category.name}
               id={course.id}
