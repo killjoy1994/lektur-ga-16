cf44c0f550790a09643e04d821de7966dcabb7fe
BagusNugrohow <papaobobo@gmail.com>
2022-03-10 21:51:22 +0700

    styling lupa password

--- src/pages/LupaPassword/LupaPassword.js
+++ src/pages/LupaPassword/LupaPassword.js
@@ -1,70 +1,62 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-
-
-// import '../../styles/lupaPassword.css';
-
+import "../../styles/lupaPassword.css";
 
 const LupaPassword = () => {
-    const[email, setEmail] = useState("");
-    const[error, setError] = useState("");
-    const[alert, setAlert] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [alert, setAlert] = useState("");
 
-    const changeEmail = (e) => {
-        setEmail(e.target.value);
-        setError('')
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+    setError("");
+  };
+
+  const send = () => {
+    if (!email) {
+      setError("Email is required!");
+    } else {
+      axios.post("https://lektur-apps.herokuapp.com/api/v1/user/forgot-password", { email: email }).then((res) => {
+        setEmail("");
+        setAlert("Please check your email");
+        setTimeout(() => {
+          setAlert("");
+        }, 1000);
+      });
     }
+  };
 
-    const send = () => {
-        if(!email) {
-            setError("Email is required!")
-        } else {
-            axios.post('https://lektur-apps.herokuapp.com/api/v1/user/forgot-password', {email: email })
-            .then(res => {
-                setEmail('')
-                setAlert('Please check your email')
-                setTimeout(() => {
-                    setAlert('')
-                }, 1000)
-            })
-        }
-    };
-
-    return (
-        <>
-            <div className='page-forgot'>
-                <div className='container'>
-                    <div className='col-md-4'>
-                        <div className='card'>
-                            <div className='title'>Forgot password?</div>
-                                <div className='card-body'>
-                                    <div className='form-group'>
-                                        <label>Email</label>
-                                        <input type="text" placeholder='Enter your email..' className='form-control' value={email} onChange={changeEmail} />
-                                    </div>
-                                    {
-                                    alert && (
-                                        <div className='alert alert-primary'>
-                                            <p>{alert}</p>
-                                        </div>
-                                    )
-                                    }
-                                    {
-                                        error && (
-                                            <div className='alert alert-danger'>
-                                                <p>{error}</p>
-                                            </div>
-                                        )
-                                    }
-                                    <button className='btn-forgot' onClick={send}>Submit</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-        </>
-    );
+  return (
+    <>
+      <div className="page-forgot">
+        <div className="container-forgot">
+          <div className="card-forgot">
+            <div className="title-forgot">Forgot password?</div>
+            <div className="card-body-forgot">
+              <div className="form-group-forgot">
+                <label className="forgot-label">Email</label>
+                <input type="text" placeholder="Enter your email.." className="form-control-forgot" value={email} onChange={changeEmail} />
+              </div>
+              {alert && (
+                <div className="alert alert-primary">
+                  <p>{alert}</p>
+                </div>
+              )}
+              {error && (
+                <div className="alert alert-danger">
+                  <p>{error}</p>
+                </div>
+              )}
+              <button className="btn-forgot" onClick={send}>
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LupaPassword;
